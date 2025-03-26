5942cf7a477e68866632a30e1301e537cfbf3080
claratg15 <161029466+claratg15@users.noreply.github.com>
2025-03-26 22:34:59 +0100

    Add files via upload

--- app FINAL dropbox.py
+++ app FINAL dropbox.py
@@ -92,7 +92,7 @@ image_labels = [f"Imatge {i+1}" for i in range(len(image_list))]
 
 # !! nou
 # Crear la disposició amb dues columnes: una per al número i una per a l'etiqueta ordenable
-col1, col2, col3, col4 = st.columns([0.3, 0.1, 0.3, 0.5])  # La primera columna és més estreta per als números
+col1, col2, col3, col4 = st.columns([0.3, 0.1, 0.4, 0.5])  # La primera columna és més estreta per als números
 
 with col2:
     st.write("<u>Rànquing</u>", unsafe_allow_html=True)
